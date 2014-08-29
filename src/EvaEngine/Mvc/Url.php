c12dcead606be48db6e6f9207fc2a26affb11b00
AlloVince <allo.vince@gmail.com>
2014-08-29 23:17:13 +0800

    Add url version for static uri

---
+++ src/EvaEngine/Mvc/Url.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Eva\EvaEngine\Mvc;
+
+use Eva\EvaEngine\Exception;
+
+class Url extends \Phalcon\Mvc\Url
+{
+    protected $version;
+
+    protected $versionFile;
+
+    public function getVersion()
+    {
+        if ($this->version) {
+            return $this->version;
+        }
+
+        $version = date('Ymd');
+        if ($this->versionFile && $fh = fopen($this->versionFile, 'r')) {
+            $version = fread($fh, 10);
+            fclose($fh);
+        }
+        return $this->version = $version;
+    }
+
+    public function setVersion($version)
+    {
+        $this->version = $version;
+        return $this;
+    }
+
+    public function setVersionFile($versionFile)
+    {
+        $this->versionFile = $versionFile;
+        return $this;
+    }
+
+    public function getStatic($url)
+    {
+        return parent::getStatic($url) . '?' . $this->getVersion();
+    }
+}
