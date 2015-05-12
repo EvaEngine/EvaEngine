525c2e149278ce6a29a780d2a5f4ba3d48f327ee
mr5 <u@mr-5.cn>
2015-05-12 14:43:14 +0800

    add helpers

--- src/EvaEngine/Helpers.php
+++ src/EvaEngine/Helpers.php
@@ -6,18 +6,87 @@
  * Time: 下午8:35
  */
 
-namespace Eva\EvaEngine;
 
-class Helpers
-{
-    static public function domain($domainName)
+use Eva\EvaEngine\IoC;
+
+if (!function_exists('p')) {
+    function p($r)
     {
-        $config = IoC::get('config');
-
-        return @$config->domains->$domainName->domain;
+        if (function_exists('xdebug_var_dump')) {
+            echo '<pre>';
+            xdebug_var_dump($r);
+            echo '</pre>';
+            //(new \Phalcon\Debug\Dump())->dump($r, true);
+        } else {
+            echo '<pre>';
+            var_dump($r);
+            echo '</pre>';
+        }
     }
+}
+if (!function_exists('dd')) {
 
-    static public function url($domainName, $baseUri = '/', $params = array(), $https = false)
+    /**
+     * 打印指定的变量并结束程序运行
+     *
+     * @param $r
+     */
+    function dd($r)
+    {
+        p($r);
+        exit();
+    }
+}
+
+/**
+ * 方便链式调用，避免过多的中间变量，例如：with(new Post())->findPosts()
+ *
+ * @param $obj
+ * @return mixed
+ */
+if (!function_exists('with')) {
+    function with($obj)
+    {
+        return $obj;
+    }
+}
+if (!function_exists('array_pluck')) {
+
+    function array_pluck($array, $itemKey, $keepItemKey = true)
+    {
+        $results = array();
+        foreach ($array as $key => $item) {
+            $itemValue = is_object($item) ? $item->$itemKey : $item[$itemKey];
+            if ($keepItemKey) {
+                $results[$key] = $itemValue;
+            } else {
+                $results[] = $itemValue;
+            }
+        }
+
+        return $results;
+    }
+}
+if (!function_exists('starts_with')) {
+
+    function starts_with($haystack, $needle)
+    {
+        // search backwards starting from haystack length characters from the end
+        return $needle === "" || strrpos($haystack, $needle, -strlen($haystack)) !== false;
+    }
+}
+if (!function_exists('ends_with')) {
+    function ends_with($haystack, $needle)
+    {
+        // search forward starting from end minus needle length characters
+        return $needle === "" || (($temp = strlen($haystack) - strlen($needle)) >= 0 && strpos($haystack, $needle,
+                $temp) !== false);
+    }
+}
+
+
+if (!function_exists('eva_url')) {
+    function eva_url($domainName, $baseUri = '/', $params = array(), $https = false)
     {
         $config = IoC::get('config');
         $domainConfig = @$config->domains->$domainName;
@@ -39,5 +108,13 @@ class Helpers
         }
 
         return $url;
+    }
+}
+if (!function_exists('eva_domain')) {
+    function eva_domain($domainName)
+    {
+        $config = IoC::get('config');
+
+        return @$config->domains->$domainName->domain;
     }
 }
