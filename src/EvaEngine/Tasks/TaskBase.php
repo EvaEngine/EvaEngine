e1a859885c66c37cbe0151d6e7ea60dfad77410d
AlloVince <allo.vince@gmail.com>
2015-10-03 23:59:24 +0800

    fix phalcon 2.0 task issue

--- src/EvaEngine/Tasks/TaskBase.php
+++ src/EvaEngine/Tasks/TaskBase.php
@@ -18,12 +18,16 @@ use Phalcon\CLI\Task;
 class TaskBase extends Task
 {
     /**
-     * @var \Eva\EvaEngine\CLI\Output\ConsoleOutput
+     * @var ConsoleOutput
      */
-    protected $output;
+    protected $_output;
 
-    public function __construct()
-    {
-        $this->output = new ConsoleOutput();
+    public function __get($name) {
+
+        //Phalcon 2.0 make __construct as final method, here use magic method to compatible
+        if ($name === 'output') {
+            return $this->_output ?: $this->_output = new ConsoleOutput();
+        }
     }
+
 }
