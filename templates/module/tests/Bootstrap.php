e064192dd0a24135958f58c639a93c7f9560a738
AlloVince <allo.vince@gmail.com>
2015-10-23 15:50:29 +0800

    unit test bootstrap psr4 fix

--- templates/module/tests/Bootstrap.php
+++ templates/module/tests/Bootstrap.php
@@ -4,6 +4,6 @@ if(!extension_loaded('phalcon')) {
 }
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = include __DIR__ . '/../../../vendor/autoload.php';
-$loader->addPsr4('<?=$moduleNamespace?>', __DIR__ . '/../src/<?=$moduleName?>');
-$loader->addPsr4('<?=$moduleNamespace?>Tests', __DIR__ . '/<?=$moduleNamespace?>Tests');
+$loader->addPsr4('<?=$moduleNamespace?>\\', __DIR__ . '/../src/<?=$moduleName?>');
+$loader->addPsr4('<?=$moduleNamespace?>Tests\\', __DIR__ . '/<?=$moduleNamespace?>Tests');
 $loader->addClassMap(['<?=$moduleNamespace?>\Module' => __DIR__ . '/../Module.php']);
