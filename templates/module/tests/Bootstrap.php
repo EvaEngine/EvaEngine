be464fad7e3db674e59a011957b6d7fbb176f96a
AlloVince <allo.vince@gmail.com>
2015-11-17 16:53:25 +0800

    fix test file path

--- templates/module/tests/Bootstrap.php
+++ templates/module/tests/Bootstrap.php
@@ -5,5 +5,5 @@ if(!extension_loaded('phalcon')) {
 /** @var Composer\Autoload\ClassLoader $loader */
 $loader = include __DIR__ . '/../../../vendor/autoload.php';
 $loader->addPsr4('<?=$moduleNamespace?>\\', __DIR__ . '/../src/<?=$moduleName?>');
-$loader->addPsr4('<?=$moduleNamespace?>Tests\\', __DIR__ . '/<?=$moduleNamespace?>Tests');
+$loader->addPsr4('<?=$moduleNamespace?>Tests\\', __DIR__ . '/<?=$moduleName?>Tests');
 $loader->addClassMap(['<?=$moduleNamespace?>\Module' => __DIR__ . '/../Module.php']);
